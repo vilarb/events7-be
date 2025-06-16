a655d7a155b88d188ebff1932c33f8e1d490afd3
Blaž Vilar <blaz.vilar12@gmail.com>
2025-06-16 10:14:38 +0200

    feat: enable cors

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.enableCors();
 
   const config = new DocumentBuilder()
     .setTitle('Events7 API')
