8031c11795f94a2f863e2f596e7a9e0b195e298b
Blaž Vilar <blaz.vilar12@gmail.com>
2025-06-17 15:16:55 +0200

    chore: cleanup and rename create event guard

--- src/events/events.controller.ts
+++ src/events/events.controller.ts
@@ -15,14 +15,14 @@ import { CreateEventDto } from './dto/create-event.dto';
 import { UpdateEventDto } from './dto/update-event.dto';
 import { Event } from './entities/event.entity';
 import { FindEventsDto } from './dto/find-events.dto';
-import { AuthGuard } from './events.guard';
+import { CreateEventGuard } from './guards/createEvent.guard';
 
 @Controller('events')
 export class EventsController {
   constructor(private readonly eventsService: EventsService) {}
 
   @Post()
-  @UseGuards(AuthGuard)
+  @UseGuards(CreateEventGuard)
   create(@Body() createUserDto: CreateEventDto) {
     return this.eventsService.create(createUserDto);
   }
